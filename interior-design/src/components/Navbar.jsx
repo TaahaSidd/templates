93b32800901deb9f1f9ba991f00c56a13ca7272f
Sidd <tahasidd33@gmail.com>
2026-06-17 17:33:19 +0530

    update file path for nav-bar

--- interior-design/src/components/Navbar.jsx
+++ interior-design/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Navbar() {
+export default function NavBar() {
     const [active, setActive] = useState('Services');
     const navLinks = ['Services', 'Portfolio', 'About', 'Contact'];
 
